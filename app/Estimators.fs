cc81b6321c944e6c4367224f1c9bf6c4593b7ae5
Stanley Sousa <stanleysousa@mail.com>
2020-06-18 03:15:38 -0300

    Adding documentation

--- app/Estimators.fs
+++ app/Estimators.fs
@@ -7,12 +7,12 @@ module Sqrt =
      ///<param name="y">The value of the random variable.</param>
      ///<param name="v">The value for which the square root will be estimated.</param>
      ///<returns>True if y is inside the rectangle.</returns>
-     let indicator y value=
-          value*y**2.0<=1.0
+     let indicator y v=
+          v*y**2.0<=1.0
 
      ///<summary>Estimates the square root of a given value using the Monte Carlo method and a uniform distribution.</summary>
      ///<param name="v">The value for which the square root will be estimated.</param>
-     ///<param name="n">number of samples.</param>
+     ///<param name="n">Number of samples.</param>
      ///<returns>Expected value for sqrt(v).</returns>
      let estimate v n =
           //Generate uniform samples with MersenneTwister
@@ -22,6 +22,5 @@ module Sqrt =
           //Filter the samples outside of the rectangle and count
           let sumG = Y |> Seq.filter(fun y -> indicator y v) |> Seq.length |> float
           
-          //Calculate the estimated value    
-          let Mn = (1.0/(float)n) * sumG
-          1.0/Mn
+          //Calculate the estimated value and returns
+          (float)n/sumG
