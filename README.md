fbd1b293fa54a44bd0b4c77188225a29bb46664f
Stanley Sousa <stanleysousa@mail.com>
2020-06-19 01:30:49 -0300

    Updating README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # montecarlo-sqrt-estimator
-Estimates the square root of a number using the Monte Carlo method
+Estimates the square root of a number using the Monte Carlo method. For more information see the [documentation](./doc/mc_sqrt.pdf).
+
+## How to use
+- You need [.NET Core 3.1](https://dotnet.microsoft.com/download/dotnet-core/3.1) runtime
+- Change [Program.fs](./app/Program.fs) as you wish, for example:
+> open Estimators\
+> ...\
+> let value, samples = 3.8 10000\
+> let sqrt3_8 = Sqrt.estimate value samples\
+> printfn "The estimated value for sqrt(%f) is %f:" value sqrt3_8\
+- On a terminal go to the [\app](../app) folder and execute "dotnet run .\MCSqrtEstimator.fsproj"
+
+## Result
+The reference value for sqrt(2.000000) is: 1.414214\
+The estimated value for sqrt(2.000000) using Monte Carlo is:\
+  1.455604 e=0.041391 from 1000 samples\
+  1.420656 e=0.006443 from 10000 samples\
+  1.411353 e=0.002861 from 100000 samples\
+  1.414597 e=0.000384 from 1000000 samples
--- app/Program.fs
+++ app/Program.fs
@@ -7,17 +7,17 @@ open Estimators.Sqrt
 let main _ =
     //number to calculate the square root
     let value = 2.0
-    let referenceValue = Math.Sqrt(value)
+    let reference = Math.Sqrt(value)
 
-    //number of samples for each simulation
+    //number of samples for each estimation
     let n1, n2, n3, n4 = 1000*1, 1000*10, 1000*100, 1000*1000
 
     //Estimates the values and print the results
     let est1, est2, est3, est4 = estimate value n1, estimate value n2, estimate value n3, estimate value n4
-    printfn "The reference value for sqrt(%f) is: %f" value referenceValue
+    printfn "The reference value for sqrt(%f) is: %f" value reference
     printfn "The estimated value for sqrt(%f) using Monte Carlo is:" value
-    printfn "  %f e=%f from %i samples" est1 (Math.Abs(est1-referenceValue)) n1
-    printfn "  %f e=%f from %i samples" est2 (Math.Abs(est2-referenceValue)) n2
-    printfn "  %f e=%f from %i samples" est3 (Math.Abs(est3-referenceValue)) n3
-    printfn "  %f e=%f from %i samples" est4 (Math.Abs(est4-referenceValue)) n4
+    printfn "  %f e=%f from %i samples" est1 (Math.Abs(est1-reference)) n1
+    printfn "  %f e=%f from %i samples" est2 (Math.Abs(est2-reference)) n2
+    printfn "  %f e=%f from %i samples" est3 (Math.Abs(est3-reference)) n3
+    printfn "  %f e=%f from %i samples" est4 (Math.Abs(est4-reference)) n4
     0 // return an integer exit code
