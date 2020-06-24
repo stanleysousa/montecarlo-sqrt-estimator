c5a8992bda3e7bf713f5fc85a134ae6d3258903a
Stanley Sousa <stanleysousa@mail.com>
2020-06-24 04:36:04 -0300

    Including plot of relative errors with xplot and google charts

--- app/Program.fs
+++ app/Program.fs
@@ -2,26 +2,51 @@
 
 open System
 open Estimators.Sqrt
+open XPlot.GoogleCharts
 
-let write v n est ref d e=
-    printfn "sqrt(%f): ref=%f est=%f delta=%f epsilon=%f for %d samples" v ref est d e n
+let log v n est ref delta epsilon=
+    printfn "sqrt(%f): ref=%f est=%f delta=%f epsilon=%f for %d samples" v ref est delta epsilon n
+
+let plot data=
+    let errors: (int * float) list = data
+
+    let options =
+        Options
+            ( title = "Erro relativo do estimador", curveType = "function",
+            width = 600,
+            height = 400,
+            vAxis = Axis(logScale = true, format="scientific", title="epsilon"),
+            hAxis = Axis(logScale = true, format="scientific", title="n") )
+
+    errors
+    |> Chart.Line
+    |> Chart.WithOptions options
+    |> Chart.Show
 
 let calculate v n=
     let est = estimate v n
     let ref = Math.Sqrt(v)
     let delta = Math.Abs(est-ref)
-    let epsilon = Math.Abs(est-ref)/est
-    write v n est ref delta epsilon
-    est, epsilon
+    let epsilon = Math.Abs(est-ref)/ref
+    v, n, est, ref, delta, epsilon
 
 [<EntryPoint>]
 let main _ =
     //number to calculate the square root
     let value = 2.0
 
-    //Estimates the values and print the results
-    for i in [1..6] do
-        let n = 10 * pown 10 i
-        calculate value n |> ignore
+    let mutable errors: (int * float) list = []
+
+    for i in [10..60] do
+        //calculate the number on samples, from 10 to 10^6, preparing to plot on a log scale
+        let n = Math.Pow(10.0, (float)i/10.0) |> Math.Ceiling |> int
+        //estimate and calculate errors
+        let v, n, est, ref, delta, epsilon = calculate value n
+        //write the result
+        log v n est ref delta epsilon
+        //add n and epsilon to the error list
+        errors <- errors @ [n, epsilon]
+
+    plot errors
 
     0 // return an integer exit code
