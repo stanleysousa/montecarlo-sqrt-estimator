e24001c6d4f9876e59c730a2666c4488af6c8b91
Stanley Sousa <stanleysousa@mail.com>
2020-06-29 00:53:16 -0300

    Fixing charts

--- app/Program.fs
+++ app/Program.fs
@@ -17,12 +17,15 @@ let main _ =
     let v = 2.0
 
     //sampling parameters
-    let maxSamples = (int)(pown 10 6)
-    let skip = 100
-    let iterations = (maxSamples/skip)
+    let x1 =[1..1..(pown 10 2)]
+    let x2 =[(pown 10 2)..10..(pown 10 3)]
+    let x3 =[(pown 10 3)..100..(pown 10 4)]
+    let x4 =[(pown 10 4)..1000..(pown 10 5)]
+    let x5 =[(pown 10 5)..10000..(pown 10 6)]
+    let x = x1 @ x2 @ x3@ x4@ x5
 
     //Estimates and generate plot data
-    let data = List.init iterations (fun i -> estimate v (i*skip))
+    let data = List.init (x |>Seq.length) (fun i -> estimate v (x.[i]))
 
     //plot result
     let title = "Erro relativo do estimador"
