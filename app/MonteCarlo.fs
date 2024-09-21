8442085fe9c096a486d5eafd82d8d24fd29f0011
Stanley Sousa <stanleysousa@mail.com>
2024-09-21 00:09:48 -0300

    improve(MonteCarlo.fs): use active patterns instead of enum for In|Out

--- app/MonteCarlo.fs
+++ app/MonteCarlo.fs
@@ -33,19 +33,21 @@ module MonteCarlo =
           [<Literal>]
           let modelName = "sqrt"
 
-          type Location = 
-               | Out = 0
-               | In = 1
+          // Active pattern for the indicator function
+          let private (|In|Out|) (v, y_i) =
+               if v * square y_i <= 1.0 then
+                    In 
+               else
+                    Out
 
           ///<summary>The indicator function based on the y random variable.</summary>
           ///<param name="v">The value for which the square root will be estimated.</param>
           ///<param name="y_i">The value of the i_th random variable.</param>
-          ///<returns>Location of the point considering the rectangle of interest.</returns>
-          let private indicator v y_i= 
-               if v * square y_i <= 1.0 then
-                    Location.In |> int
-               else 
-                    Location.Out |> int
+          ///<returns>Location of the point considering the rectangle of interest.</returns>          
+          let indicator v y_i =
+               match v, y_i with
+                    | In -> 1
+                    | Out -> 0
 
           ///<summary>Estimates the square root of 'v' using the Monte Carlo method.</summary>
           ///<param name="v">The value for which the square root will be estimated.</param>
