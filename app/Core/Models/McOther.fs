df98aa6a50796c78baaec6c1f2d7144b324adc6b
Stanley Sousa <stanleysousa@mail.com>
2024-09-24 10:27:15 -0300

    rmove unused code

--- app/Core/Models/McOther.fs
+++ app/Core/Models/McOther.fs
@@ -8,12 +8,6 @@ module McOther =
      [<Literal>]
      let private name =  "other"
 
-     [<Literal>]
-     let private inputMessage =  "What are the input constraints?"
-
-     ///<summary>Validate inputs array according to model constraints</summary>
-     let validateInputs args :  Result<array<string>, string> = Error "NotImplementedException"
-
      ///<summary>Calculates the expected value for square root of 'v'.</summary>
      let expectedValue v = infinity
 
